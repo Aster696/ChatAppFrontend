26ab4e127f511a6507106b68cc59c0ce56087381
Aster696 <asterrebeiro69@gmail.com>
2021-09-17 21:09:22 +0530

    first commit

--- src/environments/routes.ts
+++ src/environments/routes.ts
@@ -1,6 +1,6 @@
 export class routes{
-    // public url = 'https://al7chat-app.herokuapp.com'
-    public url = 'http://localhost:5700';
+    public url = 'https://al7chat-app.herokuapp.com'
+    // public url = 'http://localhost:5700';
     public webLink = 'https://al7-chat-app-cli.netlify.app';
     public mail = 'comlogix69@gmail.com';
 }
